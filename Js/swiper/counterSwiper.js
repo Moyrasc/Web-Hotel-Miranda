8a64375c943c63da53e5c1bf1ac223bd8feddfec
Moyrasc <noemisolac@gmail.com>
2023-02-08 12:02:35 +0100

    complet about mobile view

---
+++ Js/swiper/counterSwiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper-counter', {
+    direction: "horizontal",
+    //loop: true,
+    // autoplay: {
+    //     delay: 4000,
+    // },
+    centeredSlides: true,
+    breakpointsBase: 'window',
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 100,
+
+        }
+    },
+
+    pagination: {
+        el: '.swiper-counter-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+})
