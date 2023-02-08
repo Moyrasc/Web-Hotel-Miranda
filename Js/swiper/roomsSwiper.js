b099b23c1278a11ec4ea4a57f65254d12f1ec360
Moyrasc <noemisolac@gmail.com>
2023-02-08 16:12:21 +0100

    rooms List view finish

---
+++ Js/swiper/roomsSwiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper-room-list", {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 60,
+
+    // autoplay: {
+    //     delay: 5000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-room-list-next',
+        prevEl: '.swiper-button-room-list-prev',
+    },
+    pagination: {
+        el: ".swiper-button-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+});
