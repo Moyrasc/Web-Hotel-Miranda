5dd2e701b5f3eae0aeb3a51656f9a34c2b86364b
Moyrasc <noemisolac@gmail.com>
2023-02-09 18:14:07 +0100

    continue work in desktop home view

--- Js/swiper/menuSwiper.js
+++ Js/swiper/menuSwiper.js
@@ -2,14 +2,20 @@ const swiper = new Swiper(".swiper-menu", {
     direction: "horizontal",
     loop: false,
     spaceBetween: 20,
+
     // autoplay: {
     //     delay: 4000,
     // },
+
     breakpointsBase: 'container',
     breakpoints: {
         320: {
             slidesPerView: 1,
             spaceBetween: 30,
+        },
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 0,
         }
     },
     navigation: {
